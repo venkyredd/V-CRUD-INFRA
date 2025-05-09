806948eb6fb7eefdab0e8532331e5b25b6c49b5e
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-09 10:48:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "v-backend-s3"       # Replace with your actual bucket name
+    key            = "us-east-1/terraform.tfstate"   # Path to the state file within the bucket
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
 
